6f32d451c60bf475c2ae11aa0c262cef82c86242
Débora Prates <deboraseraos05@gmail.com>
2024-07-15 16:52:15 -0300

    Create exercicios01.py

---
+++ lists/exercicios01.py
@@ -0,0 +1,7 @@
+minha_lista = ["banana", "morango", "kiwi", "uva"]
+print(minha_lista)
+print(minha_lista[1])
+minha_lista.append("abacaxi")
+print(len(minha_lista))
+minha_lista.remove("uva")
+print(minha_lista)
